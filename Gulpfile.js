e639772fdd7cbc7a0dfd5f36fe784805bca6653d
Kevin Ashcraft <kevin@kevashcraft.com>
2017-07-09 17:49:45 -0400

    added around the web section. closes #2

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,15 +7,33 @@ var production = argv.devel ? '--development' : '--production';
 gulp.task('www', function () {
   gulp.src('Gulpfile-www.js')
     .pipe(chug({
-      args: [production]
+      args: [production],
+    }))
+})
+
+gulp.task('watch-www', function () {
+  gulp.src('Gulpfile-www.js')
+    .pipe(chug({
+      tasks: ['watch'],
+      args: ['--development'],
     }))
 })
 
 gulp.task('blog', function () {
   gulp.src('Gulpfile-blog.js')
     .pipe(chug({
-      args: [production]
+      args: [production],
+    }))
+})
+
+gulp.task('watch-blog', function () {
+  gulp.src('Gulpfile-blog.js')
+    .pipe(chug({
+      tasks: ['watch'],
+      args: ['--development'],
     }))
 })
 
 gulp.task('default', ['www', 'blog'])
+
+gulp.task('watch', ['watch-www', 'watch-blog'])
