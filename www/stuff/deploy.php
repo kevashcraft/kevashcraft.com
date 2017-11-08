8c5d6307a9faa7a6357d341deb2e5fe451df485d
Kevin Ashcraft <kevin@kevashcraft.com>
2017-11-08 16:35:17 -0500

    git web hook deploy sudo

--- www/stuff/deploy.php
+++ www/stuff/deploy.php
@@ -1,8 +1,9 @@
 <?php
 # add to visudo
 # apache ALL=(kevin) NOPASSWD: /usr/bin/git
+# apache ALL=(kevin) NOPASSWD: /usr/bin/gulp
 
 shell_exec('cd $(git rev-parse --show-toplevel)');
-shell_exec('git pull origin master');
-shell_exec('gulp');
+shell_exec('sudo -u kevin git pull origin master');
+shell_exec('sudo -u kevin gulp');
 
