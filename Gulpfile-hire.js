e7817fad87ad8713c0cb76a85a6b670baea88cfa
Kevin Ashcraft <kevin@kevashcraft.com>
2017-11-17 12:50:08 -0500

    hire added

---
+++ Gulpfile-hire.js
@@ -0,0 +1,94 @@
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var gulpif = require('gulp-if');
+var del = require('del');
+var sass = require('gulp-sass');
+var twig = require('gulp-twig');
+var minifyJS = require('gulp-minify');
+var minifyCSS = require('gulp-clean-css');
+var minifyHTML = require('gulp-htmlmin');
+var sequence = require('run-sequence');
+var fs = require('fs');
+var argv = require('yargs').argv;
+var sitemap = require('gulp-sitemap');
+
+var prod = argv.production ? true : false;
+
+gulp.task('clean', function(cb) {
+  return del('hire/dist');
+});
+
+// twig rendering
+gulp.task('site', function() {
+  'use strict';
+  var stream = gulp.src('hire/site/pages/**/*.twig')
+    .pipe(twig({
+      errorLogToConsole: true,
+      base: 'hire/site',
+    }))
+    .pipe(gulpif(prod, minifyHTML({
+      collapseWhitespace: true,
+    })))
+    .pipe(gulp.dest('hire/dist'));
+  return stream;
+});
+
+// sass compilation
+gulp.task('styles', function(cb) {
+  var stream = gulp.src('hire/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(prod, minifyCSS()))
+    .pipe(gulp.dest('hire/dist/styles/'));
+  return stream;
+});
+
+// minify JS
+gulp.task('scripts', function(cb) {
+  var stream = gulp.src('hire/scripts/**/*.js')
+    .pipe(gulpif(prod, minifyJS({
+      ext: {
+        min: '.js',
+      },
+      noSource: true,
+    })))
+    .pipe(gulp.dest('hire/dist/scripts/'));
+  return stream;
+});
+
+gulp.task('stuff', function(cb) {
+  return gulp.src(['hire/stuff/**/*', 'hire/stuff/**/.*', 'common/stuff/**/*', '*node_modules/font-awesome/css/**/*', '*node_modules/font-awesome/fonts/**/*', '*node_modules/chart.js/dist/Chart.min.js'])
+    .pipe(gulp.dest('hire/dist'));
+});
+
+gulp.task('sitemap', function(cb) {
+  var stream = gulp.src('hire/dist/**/*.html', {
+    read: false
+  })
+  .pipe(sitemap({
+    siteUrl: 'https://hire.kevashcraft.com'
+  }))
+  .pipe(gulp.dest('hire/dist'))
+  
+  return stream
+})
+
+gulp.task('all', ['site', 'styles', 'scripts', 'stuff'])
+
+gulp.task('default', ['clean'], function () {
+  sequence('all', 'sitemap')
+})
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch('hire/stuff/**/*.*', ['stuff']);
+  gulp.watch('hire/styles/**/*.scss', ['styles']);
+  gulp.watch('hire/scripts/**/*.js', ['scripts']);
+  gulp.watch('hire/site/**/*.twig', ['site']);
+});
+
+gulp.task('watch-styles', ['default'], function() {
+  gulp.watch('hire/styles/**/*.scss', ['styles']);
+});
+
+gulp.task('watch-scripts', ['default'], function() {
+  gulp.watch('hire/scripts/**/*.scss', ['scripts']);
+});
---
+++ hire/scripts/master.js
@@ -0,0 +1,134 @@
+var KA = {}
+window.KA = KA
+
+var ctx = document.getElementById('pie_chart').getContext('2d')
+
+KA.hatChart = new Chart(ctx, {
+  type: 'pie',
+  options: {
+    responsive: true,
+    legend: {
+      display: false,
+    },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          var label = data.labels[tooltipItem.index]
+          return ' - ' + label
+        }
+      }
+    }
+  },
+  data: {
+    datasets: [{
+      data: [100, 100, 100],
+      // http://paletton.com/#uid=73x1q0ks5C0hoLJmOFY-Yv3AYnM
+      backgroundColor: [
+        'rgb(185, 23, 185)',
+        'rgb(33, 118, 184)',
+        'rgb(255, 159, 31)',
+        // 'rgb(211, 248, 30)',
+        ]
+    }],
+    labels: [
+      'Database Admin',
+      'Full-Stack Dev',
+      'Linux Admin',
+    ],
+  },
+})
+
+KA.openTip = function(pointIndex){
+  var oChart = KA.hatChart
+   var datasetIndex = 0
+   if(oChart.tooltip._active === undefined)
+      oChart.tooltip._active = []
+   var activeElements = oChart.tooltip._active;
+   var requestedElem = oChart.getDatasetMeta(datasetIndex).data[pointIndex];
+   for(var i = 0; i < activeElements.length; i++) {
+       if(requestedElem._index == activeElements[i]._index)
+          return;
+   }
+
+   activeElements.push(requestedElem);
+   oChart.tooltip._active = activeElements;
+   oChart.tooltip.update(true);
+   oChart.draw();
+}
+
+KA.closeTips = function(){
+  KA.hatChart.tooltip._active = []
+  KA.hatChart.tooltip.update(true);
+  KA.hatChart.draw();
+}
+
+var hatTitles = document.getElementById('hat_titles').getElementsByTagName('p')
+for (var i = 0; i < hatTitles.length; i++) {
+  var element = hatTitles[i]
+
+  element.addEventListener('mouseenter', function() {
+    // var index = Array.prototype.indexOf.call(this.parentNode.children, this)
+    var index = this.dataset.index
+    KA.openTip(index)
+  })
+
+  element.addEventListener('mouseleave', function() {
+    KA.closeTips()
+  })
+
+}
+
+var tabShow = function() {
+  var tabName = this.dataset.tab || this.closest('[data-tab]').dataset.tab
+  
+  var tabContainer = this.closest('.tab-container')
+
+  // add active class to tab anchors
+  var tabAnchors = tabContainer.querySelectorAll('ul li a')
+  for (var i = 0; i < tabAnchors.length; i++) {
+    var tabAnchor = tabAnchors[i]
+    if (tabAnchor.dataset.tab === tabName) {
+      tabAnchor.parentElement.classList.add('active')
+    } else {
+      tabAnchor.parentElement.classList.remove('active')
+    }
+  }
+
+  // show/hide tabs
+  var tabs = tabContainer.querySelectorAll('div div[data-tab]')
+  for (var i = 0; i < tabs.length; i++) {
+    var tab = tabs[i]
+    if (tab.dataset.tab === tabName) {
+      tab.classList.add('active')
+    } else {
+      tab.classList.remove('active')
+    }
+  }
+}
+
+var tabContainers = document.querySelectorAll('.tab-container')
+for (var i = 0; i < tabContainers.length; i++) {
+ var tabContainer = tabContainers[i]
+ var anchors = tabContainer.querySelectorAll('ul li a')
+ for (var j = 0; j < anchors.length; j++) {
+   var anchor = anchors[j]
+   anchor.addEventListener('click', tabShow)
+ }
+}
+
+// input labels show
+Array.prototype.forEach.call(document.querySelectorAll('.input-container input, .input-container textarea'), function(el) {
+  el.addEventListener('input', function() {
+    if (this.value.length > 0) {
+      var label = document.querySelector('label:not(.active)[for="' + this.id + '"]')
+      if (label) {
+        label.classList.add('active')
+      }
+    } else {
+      var label = document.querySelector('label.active[for="' + this.id + '"]')
+      if (label) {
+        label.classList.remove('active')
+      }
+    }
+  })
+})
